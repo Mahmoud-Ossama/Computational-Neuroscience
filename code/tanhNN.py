05637188050774cb5b71b2d2de85b894071c232c
Mahmoud <vip.m.osama@gmial.com>
2025-02-25 21:22:05 +0200

    Lec 1 Update assin

--- code/tanhNN.py
+++ code/tanhNN.py
@@ -1,25 +1,36 @@
 import numpy as np
 
+np.random.seed(42)
+
+w1 = np.random.uniform(-0.5, 0.5, (3, 2)) 
+w2 = np.random.uniform(-0.5, 0.5, (2, 1)) 
+
+b1 = 0.5 
+b2 = 0.7 
+
+x = np.array([0.1, 0.2, 0.3])
+
 def tanh(x):
     return np.tanh(x)
 
-np.random.seed(42)
+# forwrd function
+def forward(x, w1, b1, w2, b2):
 
-w1 = np.random.uniform(-0.5, 0.5, (2, 2))
-w2 = np.random.uniform(-0.5, 0.5, (1, 2))
+    z1 = np.dot(x, w1) + b1
+    a1 = tanh(z1)
+    
 
-b1 = 0.5
-b2 = 0.7
+    z2 = np.dot(a1, w2) + b2
+    a2 = tanh(z2)
+    
+    return a1, a2
 
-x = np.array([[0.1, 0.2]])
+hidden_output, final_output = forward(x, w1, b1, w2, b2)
 
-z1 = np.dot(x, w1) + b1
-a1 = tanh(z1)
-
-z2 = np.dot(a1, w2.T) + b2
-output = tanh(z2)
-
-print("Weights (w1):\n", w1)
-print("Weights (w2):\n", w2)
-print("Hidden layer output:\n", a1)
-print("Network output:\n", output)
+print("Input:", x)
+print("\nWeights (hidden layer):\n", w1)
+print("\nWeights (output layer):\n", w2)
+print("\nBias (hidden layer):", b1)
+print("\nBias (output layer):", b2)
+print("\nHidden layer output:", hidden_output)
+print("\nFinal output:", final_output[0])
